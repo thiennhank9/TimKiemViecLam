e12123f112066187101acb19e2d9fb2291e7ec84
nhan <thiennhank9@gmail.com>
2018-10-14 17:00:20 +0700

    #Upload project

---
+++ js/nga-custom-javascript.js
@@ -0,0 +1,4 @@
+function myFunction () {
+    document.getElementById("nga-custom-close-after-singin").style.display = "none"
+    document.getElementById("nga-custom-open-after-singin").style.display ="block"
+};
